66a10400a4403ddfab979659da31ab1f118fc7de
Nikolaj Bræmer Christensen <354322@VIAUC.DK>
2024-10-03 10:53:22 +0200

    Opgave besvarelse for CarFerry

--- Program.cs
+++ Program.cs
@@ -1,10 +1,65 @@
-﻿namespace opgaver
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+
+namespace Lektion_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CarFerry færge = new CarFerry("M/F Fenja", 5000, 15000);
+
+            Vehicle Bil1 = new Vehicle("ZZ27472", 2300);
+            Vehicle Bil2 = new Vehicle("CT27291", 1700);
+            Vehicle Bil3 = new Vehicle("DV345662", 2500);
+            Vehicle Bil4 = new Vehicle("av442123", 5300);
+
+            færge.addVehicle(Bil1);
+            færge.addVehicle(Bil2);
+            færge.addVehicle(Bil3);
+
+
+            Console.WriteLine(færge.GetName() + " som vejer " + færge.GetWeight() + "kg");
+            Console.WriteLine("har en maks kapacitet på " + færge.GetMaxWeight() + "kg");
+            Console.WriteLine("sejler fra Esbjerg til Fanø med billerne");
+            Console.WriteLine(Bil1.ToString());
+            Console.WriteLine(Bil2.ToString());
+            Console.WriteLine(Bil3.ToString());
+
+
+            Console.WriteLine("samlet har billerne og færgen en vægt på " + færge.GetTotalWeight());
+
+            if (færge.IsOverWeight())
+            {
+                Console.WriteLine(færge.GetName() + " er overbelastet!");
+            }
+            else
+            {
+                Console.WriteLine(færge.GetName() + " er ikke overbelastet.");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Når de rammer fanø sætter " + færge.GetName() + " en bil af og samler en anden op");
+            færge.RemoveVehicle(Bil2);
+            færge.addVehicle(Bil4);
+
+            Console.WriteLine(færge.GetName());
+            Console.WriteLine("maks kapacitet på " + færge.GetMaxWeight() + "kg");
+            Console.WriteLine("sejler tilbage til Esbjerg fra Fanø med billerne");
+            Console.WriteLine(Bil1.ToString());
+            Console.WriteLine(Bil3.ToString());
+            Console.WriteLine(Bil4.ToString());
+            Console.WriteLine("samlet har billerne og færgen en vægt på " + færge.GetTotalWeight());
+
+            if (færge.IsOverWeight())
+            {
+                Console.WriteLine(færge.GetName() + " er overbelastet!");
+            }
+            else
+            {
+                Console.WriteLine(færge.GetName() + " er ikke overbelastet.");
+            }
         }
     }
 }
---
+++ Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lektion_7
+{
+    public class Vehicle
+    {
+        private String licensePlate;
+        private double weight;
+
+        public String getLicensePlate()
+        {
+            return licensePlate;
+        }
+
+        public double getWeight()
+        {
+            return weight;
+        }
+
+        public Vehicle(String licensePlate, double weight)
+        {
+            this.licensePlate = licensePlate;
+            this.weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return "bil [Nummerplade: " + licensePlate + " vægt: " + weight + "]";
+        }
+
+        public bool Equals(Vehicle other)
+        {
+            if (other == null) return false;
+            return this.licensePlate == other.licensePlate;
+        }
+    }
+}
+
+
+
